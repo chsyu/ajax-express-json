0bef33ceae564eead3263626a1bc63ce8c4b2abe
chi-shan <chisamyu@seed.net.tw>
2016-01-14 21:49:57 +0800

    rev0

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+
+app.post('/endpoint', function(req, res){
+	var obj = {};
+	console.log('body: ' + JSON.stringify(req.body));
+	res.send(req.body);
+});
+
+
+app.listen(3000);
